ec97bef761f0bab3840380ed06f9c7b7f9e39b7a
xuewenjun <948361833Wqq.com>
2017-09-15 18:15:06 +0800

    esline config modify

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,4 @@
 // http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
   parser: 'babel-eslint',
@@ -9,7 +8,7 @@ module.exports = {
   env: {
     browser: true,
   },
-  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
   extends: 'standard',
   // required to lint *.vue files
   plugins: [
@@ -21,7 +20,15 @@ module.exports = {
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
-    // allow debugger during development
+    // allow debugger during development,
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // 语句强制分号结尾
+    "semi": [1, "always"],
+    //强制驼峰法命名
+    "camelcase": 1,
+    //函数定义时括号前面要不要有空格
+    "space-before-function-paren": [0, "always"],
+    //建议使用单引号
+    "quotes": [1, "single"],
   }
-}
+};
